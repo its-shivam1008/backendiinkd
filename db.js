c806aaa78392540953443d495423590151bb90f5
its-shivam1008 <119884846+its-shivam1008@users.noreply.github.com>
2024-03-07 20:54:17 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://saboteurshivam:bCk6ID2e39JaADCR@cluster0.cozu8rw.mongodb.net/";
+const mongoURI = "mongodb+srv://saboteurshivam:QWERTY12345@cluster0.cozu8rw.mongodb.net/";
 const connectToMongo = () =>{
     mongoose.connect(mongoURI);
     console.log("Successfully connected to Mongo")
